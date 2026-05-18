c16e803e1b0d2f3fe330cd06750820df7f5f52a8
qwerty吃小庄 <git@qwertycxz.top>
2026-05-18 14:37:53 +0800

    chore: 最小实现

--- main.py
+++ main.py
@@ -6,56 +6,16 @@ from os import environ
 
 from agentscope.agent import ReActAgent
 from agentscope.formatter import OpenAIChatFormatter
-from agentscope.mcp import HttpStatelessClient
-from agentscope.message import Msg, TextBlock
+from agentscope.message import Msg
 from agentscope.model import OpenAIChatModel
 from agentscope.pipeline import stream_printing_messages
-from agentscope.tool import ToolResponse, Toolkit
 from agentscope_runtime.engine.app import AgentApp
 from agentscope_runtime.engine.schemas.agent_schemas import AgentRequest, AgentResponse
 from agentscope_runtime.engine.tracing.base import EventContext
 from fastapi import FastAPI
 from tiktoken import get_encoding
 
-from cst import CSTKnowledgeBase
-
 LOGGER = getLogger('智能体')
-
-# 本地工具调用函数，必须提供详细的 docstring（就是这些三引号括起来的注释），以便智能体能够正确使用工具
-def calculate(operator: str, operand1: float, operand2: float):
-	'''
-	四则运算计算器
-
-	Args:
-		operator (str): 运算符，可为加（+）、减（-）、乘（*）、除（/）
-		operand1 (float): 操作数1
-		operand2 (float): 操作数2
-	'''
-	result: float
-	match operator:
-		case '+':
-			result = operand1 + operand2
-		case '-':
-			result = operand1 - operand2
-		case '*':
-			result = operand1 * operand2
-		case '/':
-			result = operand1 / operand2
-		case _:
-			raise ValueError(f'运算符不支持: {operator}')
-	return ToolResponse([
-		TextBlock(text = f'{result}', type = 'text')
-	])
-
-# 科技云知识库查询，以本地工具调用的形式提供给智能体使用；如要使用多个知识库，可以创建多个 CSTKnowledgeBase 实例，并注册到 toolkit 中
-knowledge = CSTKnowledgeBase('', '')
-
-# MCP 客户端，请按需调整，参考文档：https://doc.agentscope.io/zh_CN/tutorial/task_mcp.html
-mcp = HttpStatelessClient('MCP', 'sse', '', {
-	'Authorization': 'Bearer '
-})
-
-toolkit = Toolkit()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -65,12 +25,6 @@ async def lifespan(app: FastAPI):
 	Args:
 		app: FastAPI应用实例
 	'''
-	# 一般本地工具
-	toolkit.register_tool_function(calculate)
-	# 科技云知识库，可自行微调提示词
-	toolkit.register_tool_function(knowledge.retrieve_knowledge, func_description = '知识库\n名称：\n描述：', func_name = 'knowledge')
-	# 外部 MCP
-	await toolkit.register_mcp_client(mcp)
 	yield
 
 # 这里执行一些初始化工作
@@ -79,6 +33,8 @@ encoding = get_encoding('o200k_base')
 formatter = OpenAIChatFormatter()
 model = OpenAIChatModel(environ['OPENAI_MODEL_NAME'], environ['OPENAI_API_KEY'], client_kwargs = {
 	'base_url': environ['OPENAI_BASE_URL'],
+}, generate_kwargs = {
+	'temperature': 0,
 })
 
 # `@app.query()` 默认使用的是 `POST /process`，并且使用的是 AgentScope 自己的一套框架，文档在 https://runtime.agentscope.io/v1.1.0/zh/protocol.html
@@ -94,7 +50,7 @@ async def query(self: AgentApp, msgs: Iterable[Msg], request: AgentRequest, resp
 		trace_event: EventContext对象，用于记录智能体处理过程中的事件和日志
 	'''
 	# 每次都创建一个新 Agent，保证每次请求都是独立的；如果需要在请求之间共享一些状态，可以考虑使用 Agent.memory 或者其他持久化存储，见 https://doc.agentscope.io/zh_CN/tutorial/task_memory.html
-	agent = ReActAgent('生态环境智能体', '你是生态环境智能体', model, formatter, toolkit)
+	agent = ReActAgent('生态环境智能体', '你是生态环境智能体', model, formatter)
 	# 如果不需要输出控制台日志，可以关闭；如果需要调试，可以打开
 	agent.set_console_output_enabled(False)
 	try:
